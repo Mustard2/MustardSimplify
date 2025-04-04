6d7a45663e12493db028226bccf61373c970ea6e
Mustard2 <mustard_sfm@protonmail.com>
2025-04-05 00:17:20 +0200

    New Data Removal tool

--- tools/ops_data_removal.py
+++ tools/ops_data_removal.py
@@ -3,8 +3,41 @@ from bpy.props import *
 from .. import __package__ as base_package
 
 
+class MustardSimplify_DataRemoval_Entry(bpy.types.PropertyGroup):
+    name: StringProperty(default="")
+    remove: BoolProperty(default=False)
+    count: IntProperty()
+
+
+class MustardSimplify_DataRemoval(bpy.types.PropertyGroup):
+    entries: CollectionProperty(type=MustardSimplify_DataRemoval_Entry)
+
+
+class MUSTARDSIMPLIFY_OT_DataRemoval_SelectAll(bpy.types.Operator):
+    """Select/deselect all data blocks"""
+    bl_idname = "mustard_simplify.data_removal_select"
+    bl_label = "Select/Deselect All"
+    bl_options = {'UNDO'}
+
+    select: BoolProperty()
+
+    @classmethod
+    def poll(cls, context):
+        return True
+
+    def execute(self, context):
+
+        scene = context.scene
+        entries = scene.MustardSimplify_DataRemoval.entries
+
+        for e in entries:
+            e.remove = self.select
+
+        return {'FINISHED'}
+
+
 class MUSTARDSIMPLIFY_OT_DataRemoval(bpy.types.Operator):
-    """Remove heavy data from objects"""
+    """Remove data from objects.\nThis is a highly destructive operation, only remove data that you know are not needed!"""
     bl_idname = "mustard_simplify.data_removal"
     bl_label = "Remove Data"
     bl_options = {'UNDO'}
@@ -33,59 +66,32 @@ class MUSTARDSIMPLIFY_OT_DataRemoval(bpy.types.Operator):
                 return 0
 
         scene = context.scene
+        entries = scene.MustardSimplify_DataRemoval.entries
         addon_prefs = bpy.context.preferences.addons[base_package].preferences
 
-        if addon_prefs.debug:
-            print("\n ----------- MUSTARD SIMPLIFY DATA REMOVAL LOG -----------\n")
-
-        # Decide which data to remove
-        to_remove = []
-        to_preserve = []
-        if self.remove_custom_string_data != "" and addon_prefs.advanced:
-            to_remove.append(self.remove_custom_string_data)
-            if addon_prefs.debug:
-                print("Removing data with string: " + self.remove_custom_string_data)
-        if self.remove_diffeomorphic_data:
-            to_remove.append("Daz")
-            if addon_prefs.debug:
-                print("Removing Diffeomorphic data")
-            if self.remove_diffeomorphic_data_preserve_morphs:
-                to_preserve.append("DazExpressions")
-                to_preserve.append("DazFacs")
-                to_preserve.append("DazFacsexpr")
-                to_preserve.append("DazFlexions")
-                to_preserve.append("DazUnits")
-                to_preserve.append("DazVisemes")
-                to_preserve.append("DazMorphCats")
-                to_preserve.append("DazCustomMorphs")
-                to_preserve.append("DazCustom")
-                to_preserve.append("DazActivated")
-
-        if not len(to_remove):
+        if not len([x for x in entries if x.remove]):
             self.report({'WARNING'}, "Mustard Simplify - No Data Block to remove was selected.")
             return {'FINISHED'}
 
-        # Gather Objects
-        objects = []
-        for obj in scene.objects:
-            objects.append(obj)
-            if obj.data is not None:
-                objects.append(obj.data)
-
         # Remove data
         data_deleted = 0
-        for obj in objects:
+        for obj in [x for x in scene.objects if x is not None]:
             items_to_remove = []
             for k, v in obj.items():
-                for el in to_remove:
-                    if el in k:
+                for ll in [x.name for x in entries if x.remove]:
+                    if ll == k:
                         items_to_remove.append(k)
+            if obj.data is not None:
+                for k, v in obj.data.items():
+                    for ll in [x.name for x in entries if x.remove]:
+                        if ll == k:
+                            items_to_remove.append(k)
             items_to_remove.reverse()
             if addon_prefs.debug and len(items_to_remove) > 0:
                 print("\n Removing from Object: " + obj.name)
             for k in items_to_remove:
-                if not k in to_preserve:
-                    data_deleted = data_deleted + remove_data(obj, k)
+                data_deleted = data_deleted + remove_data(obj, k)
+                data_deleted = data_deleted + remove_data(obj.data, k)
                 if addon_prefs.debug:
                     print("   - " + k)
             obj.update_tag()
@@ -99,32 +105,86 @@ class MUSTARDSIMPLIFY_OT_DataRemoval(bpy.types.Operator):
 
     def invoke(self, context, event):
 
-        return context.window_manager.invoke_props_dialog(self, width=400)
+        def add_entry(collection, name):
+            for c in collection:
+                if c.name == name:
+                    c.count += 1
+                    return False
+
+            add_item = collection.add()
+            add_item.name = name
+            add_item.count = 1
+            return True
+
+        scene = bpy.context.scene
+        entries = scene.MustardSimplify_DataRemoval.entries
+        addon_prefs = bpy.context.preferences.addons[base_package].preferences
+
+        entries.clear()
+
+        for o in [x for x in scene.objects if x is not None]:
+            rna_props = o.bl_rna.properties
+            for k in o.keys():
+                if k in rna_props and rna_props[k].is_runtime:
+                    add_entry(entries, k)
+            if o.data is not None:
+                rna_props = o.data.bl_rna.properties
+                for k in o.data.keys():
+                    if k in rna_props and rna_props[k].is_runtime:
+                        add_entry(entries, k)
+
+        return context.window_manager.invoke_props_dialog(self, width=1024 if addon_prefs.debug else 900)
 
     def draw(self, context):
 
-        addon_prefs = bpy.context.preferences.addons[base_package].preferences
+        scene = context.scene
+        entries = scene.MustardSimplify_DataRemoval.entries
 
         layout = self.layout
+        box = layout.box()
 
-        if addon_prefs.advanced:
-            box = layout.box()
-            box.label(text="General", icon="SETTINGS")
-            box.prop(self, 'remove_custom_string_data')
+        if len(entries) == 0:
+            box.label(text="No Data Blocks found", icon="ERROR")
+            return
+
+        box.label(text="This operation is highly destructive! Remove only data-blocks you not need!", icon="ERROR")
+
+        ordered_entries = sorted([x for x in entries], key=lambda x: x.count, reverse=True)
+        length_entries = len(ordered_entries)
+
+        cols = [0, 0, 0]
+        for i in range(len(cols)):
+            cols[i] = int(length_entries/len(cols)) + (cols[i-1] if i > 0 else 0)
+        remainder = (length_entries % cols[2]) if cols[2] > 0 else 1
+        cols[0] += remainder
+        cols[1] += remainder
+        cols[2] += remainder
 
         box = layout.box()
-        col = box.column()
-        col.label(text="External Add-on", icon="WORLD_DATA")
-        col.prop(self, 'remove_diffeomorphic_data')
-        row = col.row(align=True)
-        row.enabled = self.remove_diffeomorphic_data
-        row.label(text="", icon="BLANK1")
-        row.prop(self, 'remove_diffeomorphic_data_preserve_morphs')
+        row = box.row(align=True)
+        row.operator("mustard_simplify.data_removal_select", text="Select All").select = True
+        row.operator("mustard_simplify.data_removal_select", text="Deselect All").select = False
+
+        row = box.row()
+        for i in range(len(cols)):
+            col = row.column()
+            begin = cols[i-1] if i > 0 else 0
+            end = cols[i]
+            for e in ordered_entries[begin:end]:
+                col.prop(e, 'remove', text=e.name + " (" + str(e.count) + ")")
 
 
 def register():
+    bpy.utils.register_class(MustardSimplify_DataRemoval_Entry)
+    bpy.utils.register_class(MustardSimplify_DataRemoval)
+    bpy.utils.register_class(MUSTARDSIMPLIFY_OT_DataRemoval_SelectAll)
+    bpy.types.Scene.MustardSimplify_DataRemoval = PointerProperty(type=MustardSimplify_DataRemoval)
     bpy.utils.register_class(MUSTARDSIMPLIFY_OT_DataRemoval)
 
 
 def unregister():
     bpy.utils.unregister_class(MUSTARDSIMPLIFY_OT_DataRemoval)
+    del bpy.types.Scene.MustardSimplify_DataRemoval
+    bpy.utils.unregister_class(MUSTARDSIMPLIFY_OT_DataRemoval_SelectAll)
+    bpy.utils.unregister_class(MustardSimplify_DataRemoval)
+    bpy.utils.unregister_class(MustardSimplify_DataRemoval_Entry)
