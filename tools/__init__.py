0b7cf4f43dd93146c1a8c985f9bae5f5d5bbca48
Mustard2 <mustard_sfm@protonmail.com>
2026-05-11 20:06:51 +0200

    Cleanup

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,13 +1,13 @@
-from . import ops_data_removal, ops_fast_normals, ops_mannequin
+from . import ops_data_removal, ops_fast_normals, ops_proxy
 
 
 def register():
     ops_data_removal.register()
     ops_fast_normals.register()
-    ops_mannequin.register()
+    ops_proxy.register()
 
 
 def unregister():
-    ops_mannequin.unregister()
+    ops_proxy.unregister()
     ops_fast_normals.unregister()
     ops_data_removal.unregister()
