d3d92563cdad8b377d8219a9f68a864cedb734af
Mustard2 <mustard_sfm@protonmail.com>
2026-06-26 23:46:47 +0200

    feature (#67): scene complexity analyzer

--- menu/__init__.py
+++ menu/__init__.py
@@ -4,16 +4,23 @@ class MainPanel:
     bl_category = "Simplify"
 
 
-from . import menu_camera_hide, menu_simplify, menu_tools  # noqa: E402
+from . import (  # noqa: E402
+    menu_camera_hide,
+    menu_complexity_analyzer,
+    menu_simplify,
+    menu_tools,
+)
 
 
 def register():
     menu_simplify.register()
     menu_camera_hide.register()
+    menu_complexity_analyzer.register()
     menu_tools.register()
 
 
 def unregister():
     menu_tools.unregister()
+    menu_complexity_analyzer.unregister()
     menu_camera_hide.unregister()
     menu_simplify.unregister()
--- tools/__init__.py
+++ tools/__init__.py
@@ -1,13 +1,15 @@
-from . import ops_data_removal, ops_fast_normals, ops_proxy
+from . import ops_complexity_analyzer, ops_data_removal, ops_fast_normals, ops_proxy
 
 
 def register():
     ops_data_removal.register()
     ops_fast_normals.register()
     ops_proxy.register()
+    ops_complexity_analyzer.register()
 
 
 def unregister():
+    ops_complexity_analyzer.unregister()
     ops_proxy.unregister()
     ops_fast_normals.unregister()
     ops_data_removal.unregister()
