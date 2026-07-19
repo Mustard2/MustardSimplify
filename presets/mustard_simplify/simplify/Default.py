48bc7c3af7f0f893be1ef6a437f166cb86d50b95
Mustard2 <mustard_sfm@protonmail.com>
2026-07-19 13:42:17 +0200

    fix: add missing presets

---
+++ presets/mustard_simplify/simplify/Default.py
@@ -0,0 +1,36 @@
+import bpy
+
+settings = bpy.context.scene.MustardSimplify_Settings
+rd = bpy.context.scene.render
+cscene = bpy.context.scene.cycles if hasattr(bpy.context.scene, "cycles") else None
+
+# Mustard Simplify's out-of-the-box defaults
+settings.blender_simplify = True
+settings.objects = False
+settings.culling = False
+settings.modifiers = True
+settings.shape_keys = True
+settings.shape_keys_disable_not_null = True
+settings.shape_keys_disable_with_drivers = True
+settings.shape_keys_disable_with_drivers_not_null = False
+settings.shape_keys_disable_with_keyframes = False
+settings.drivers = True
+settings.physics = True
+
+rd.simplify_subdivision = 6
+rd.simplify_child_particles = 1.0
+rd.simplify_volumes = 1.0
+rd.use_simplify_normals = False
+rd.simplify_subdivision_render = 6
+rd.simplify_child_particles_render = 1.0
+
+if cscene is not None:
+    cscene.texture_limit = "OFF"
+    cscene.texture_limit_render = "OFF"
+    cscene.use_camera_cull = False
+    cscene.camera_cull_margin = 0.1
+    cscene.use_distance_cull = False
+    cscene.distance_cull_margin = 50.0
+    if hasattr(cscene, "texture_resolution"):
+        cscene.texture_resolution = 100.0
+        cscene.texture_resolution_render = 100.0
